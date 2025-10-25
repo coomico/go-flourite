1b6e73a55877449a10e399531ab6ac548e036a01
coomico <69asdfghjkl00@gmail.com>
2025-10-25 22:10:29 +0700

    fix: adding and improve CSS pattern

--- language_css.go
+++ language_css.go
@@ -5,7 +5,18 @@ import "regexp"
 var css = []languagePattern{
 	// properties check
 	// [!] the original one isn't compatible with Golang regexp std library
-	{expression: regexp.MustCompile(`[a-z-]+:\s*[^:;]+;`), patternType: keyword},
+	{expression: regexp.MustCompile(`^\s*[a-z-]+:\s*[^:;{}\[\]]+;?`), patternType: keyword},
+
+	// selector
+	{expression: regexp.MustCompile(`^[a-zA-Z][\w-]*(\[[^\]]+\])?(\:[a-z-]+)?\s*[,{]`), patternType: keyword},
+
+	// specific property:value
+	{
+		expression:  regexp.MustCompile(`(border|margin|padding|background|color|display|position|width|height|font|text)(-[a-z]+)?:\s*[^:;]+;?`),
+		patternType: keyword,
+	},
+
+	{expression: regexp.MustCompile(`/\*[\s\S]*?\*/`), patternType: commentBlock},
 
 	// <style> tag from HTML
 	{expression: regexp.MustCompile(`<(\/)?style>`), patternType: not},
