71d56aa0c93fed61e409ab748f8c557155613d15
coomico <69asdfghjkl00@gmail.com>
2025-10-25 23:45:38 +0700

    fix: add and improve HTML patterns

--- language_html.go
+++ language_html.go
@@ -5,8 +5,14 @@ import "regexp"
 var html = []languagePattern{
 	{expression: regexp.MustCompile(`<!DOCTYPE (?:html|HTML PUBLIC .+)>`), patternType: metaModule, nearTop: true},
 
+	// HTML-specific tags
+	{expression: regexp.MustCompile(`<(html|head|body|title|meta|link)(\s|>)`), patternType: keyword},
+
 	// tags
-	{expression: regexp.MustCompile(`<[a-z0-9]+(?:\s*[\w]+=(?:'|").+(?:'|")\s*)?>.+?</[a-z0-9]+>`), patternType: keyword},
+	{expression: regexp.MustCompile(`<[a-z][a-z0-9]*(?:\s+[\w-]+=(?:'[^']*'|"[^"]*"))*\s*/?>`), patternType: keyword},
+
+	// closing tags separately
+	{expression: regexp.MustCompile(`</[a-z][a-z0-9]*>`), patternType: keyword},
 
 	// comments
 	{expression: regexp.MustCompile(`<!--(.*)(?:-->)?`), patternType: commentBlock},
