37f6ca4b22ce8bff7d09dc3e4346c357aecefed7
coomico <69asdfghjkl00@gmail.com>
2025-10-26 20:34:52 +0700

    fix: add and improve Julia and Kotlin patterns

--- language_julia.go
+++ language_julia.go
@@ -3,31 +3,41 @@ package flourite
 import "regexp"
 
 var julia = []languagePattern{
-	{expression: regexp.MustCompile(`(using)\s\w+`), patternType: metaImport},
-	{expression: regexp.MustCompile(`(bare\s)?module`), patternType: metaModule},
+	{expression: regexp.MustCompile(`\busing\s+\w+`), patternType: metaImport},
+	{expression: regexp.MustCompile(`\b(bare\s+)?module\b`), patternType: metaModule},
+	{expression: regexp.MustCompile(`function\s+\w+\s*\([^)]*\)\s*\n`), patternType: keywordFunction},
 
-	// avoiding Python's import
-	{expression: regexp.MustCompile(`from\s.+import\s.+`), patternType: not},
+	// new{T} constructor
+	{expression: regexp.MustCompile(`\bnew\{[^}]+\}`), patternType: keywordOther},
 
 	// stdout / print line
 	{expression: regexp.MustCompile(`println\(.*\)`), patternType: keywordPrint},
 
-	{expression: regexp.MustCompile(`(.*)!\(.*\)`), patternType: macro},
-	{expression: regexp.MustCompile(`for\s(\w+)\s(in|=)\s`), patternType: keywordControl},
+	{expression: regexp.MustCompile(`\w+!\s*\(`), patternType: macro},
+	{expression: regexp.MustCompile(`for\s+\w+\s+(in|=)\s+`), patternType: keywordControl},
+	{expression: regexp.MustCompile(`while\s+[^{]+\n`), patternType: keywordControl},
+	{expression: regexp.MustCompile(`\breturn\s+`), patternType: keywordControl},
 
-	// function isn't followed by {
-	{expression: regexp.MustCompile(`function\s\w+\(.*\)\s\{`), patternType: not},
-
-	// while loop isn't followed by {
-	{expression: regexp.MustCompile(`while\s+\(.+\)\n`), patternType: not},
+	{expression: regexp.MustCompile(`Union\{[^}]+\}`), patternType: keywordVariable},
 
 	{expression: regexp.MustCompile(`end\n?`), patternType: keyword},
 
 	// struct with <: annotation
-	{expression: regexp.MustCompile(`struct\s(.*)\s<:\s`), patternType: keywordOther},
+	{expression: regexp.MustCompile(`(mutable\s+)?struct\s+\w+(\{[^}]+\})?`), patternType: keywordOther},
 
 	{expression: regexp.MustCompile(`(::)?(Int|Uint)(8|16|32|64|128)`), patternType: keywordVariable},
-	{expression: regexp.MustCompile(`[0-9]+im`), patternType: keyword},
+	{expression: regexp.MustCompile(`\b(::)?Float(16|32|64)\b`), patternType: keywordVariable},
+	{expression: regexp.MustCompile(`[0-9]+im\b`), patternType: keyword},
+	{expression: regexp.MustCompile(`\bNothing\b`), patternType: constantNull},
+	{expression: regexp.MustCompile(`\bSubString\s*\(`), patternType: keywordOther},
+	{expression: regexp.MustCompile(`\blength\s*\(`), patternType: keywordOther},
+
+	// array comprehension and indexing
+	{expression: regexp.MustCompile(`\[\w+\s+for\s+\w+\s+in\s+`), patternType: keywordControl},
+	{expression: regexp.MustCompile(`\w+\[(\d+|end|\d+:\d+|:|\d+:end|end-\d+)\]`), patternType: keywordOther},
+
+	// avoiding Python's import
+	{expression: regexp.MustCompile(`from\s.+import\s.+`), patternType: not},
 
 	// avoiding Rust confusion
 	{expression: regexp.MustCompile(`\{:\?\}`), patternType: not},
